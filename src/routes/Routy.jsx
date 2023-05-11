6d15cf38fa99341833fc1fd0e745a9f298fec260
bukason20 <chukwuebukaedward20@gmail.com>
2023-05-11 10:55:47 +0100

    changing and adding routes to the homePage and Erropage

--- src/routes/Routy.jsx
+++ src/routes/Routy.jsx
@@ -5,15 +5,18 @@ import {
     RouterProvider
       } from "react-router-dom";
 import NavLayout from "../components/NavLayout/NavLayout";
+import Homepage from "../pages/Homepage/Homepage";
+import NotFound from "../pages/NotFound/NotFound";
 
     const Routy = () => {
     
       const router = createBrowserRouter(
         createRoutesFromElements(
           <Route path='/' element={<NavLayout />}>
-
-
-          </Route>
+            <Route index element = {<Homepage />} />
+            <Route path = "*" element = {<NotFound />}/>
+        </Route>
+      
         )
       )
       return (
