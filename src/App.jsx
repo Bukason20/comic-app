d2181484eabeeeba64941b688b6562c624dfc643
bukason20 <chukwuebukaedward20@gmail.com>
2023-05-11 10:52:19 +0100

    removed the Error Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Footer from './components/Footer/Footer'
 import Routy from './routes/Routy'
-import NotFound from './pages/NotFound/NotFound'
 
 
 const App = () => {
   return (
     <div>
       <Routy />
-      <NotFound />
       <Footer />
     </div>
   )
